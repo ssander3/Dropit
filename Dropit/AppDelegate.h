e9365def9e23c5bf4daaf93cc506a715bcaa921a
Scott Sanders <scott@thesandersfam.com>
2015-02-18 22:43:00 -0600

    Initial Commit

---
+++ Dropit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dropit
+//
+//  Created by Scott Sanders on 2/18/15.
+//  Copyright (c) 2015 Scott Sanders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dropit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dropit
+//
+//  Created by Scott Sanders on 2/18/15.
+//  Copyright (c) 2015 Scott Sanders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
